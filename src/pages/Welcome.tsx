4e3a20f83a99bfa4317d5fd15e1920625e240206
Jefferson Ferreira <jefferson14489@gmail.com>
2021-04-19 11:33:52 -0300

    Initial commit - Semana nlw 5 - page welcome - configuracoes

---
+++ src/pages/Welcome.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { Text, Image, TouchableOpacity, StyleSheet, SafeAreaView } from 'react-native';
+import colors from '../../styles/colors';
+
+import wateringImg from '../assets/watering.png';
+import Button from '../components/Button';
+
+const Welcome = () => {
+    return (
+        <SafeAreaView style={styles.container}>
+            <Text style={styles.title}>
+                Gerencie {'\n'}
+                suas plantas {'\n'}
+                de forma fácil
+            </Text>
+
+            <Image source={wateringImg} style={styles.image} />
+
+            <Text style={styles.subTitle}>
+                Não esqueça mais de regar suas plantas. Nós cuidamos de lembrar você
+                sempre que precisar.
+            </Text>
+
+            <Button title="Avançar" />
+        </SafeAreaView>
+    );
+}
+
+export default Welcome;
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'space-between',
+    },
+    title: {
+        fontSize: 32,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        color: colors.heading,
+        marginTop: 38,
+    },
+    subTitle: {
+        textAlign: 'center',
+        fontWeight: 'bold',
+        fontSize: 18,
+        paddingHorizontal: 20,
+        color: colors.heading
+    },
+    image: {
+        width: 292,
+        height: 284
+    },
+})
